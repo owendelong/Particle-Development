d926a661de273ae64bd89eb6049fbdecc724c081
Owen DeLong <owen@delong.com>
2015-08-21 21:08:03 -0700

    Attempted to fix RegisterHandler -- Failed

--- Projects/PostTempHumV2/PostTempHumV2.h
+++ Projects/PostTempHumV2/PostTempHumV2.h
@@ -3,7 +3,11 @@
 typedef bool(*FunctionPointer)();
 extern FunctionPointer *data_collectors; // At time of writing only need to support 2 sensor families.
 extern uint8_t HandlerCount=0;
-#define RegisterHandler(NAME, HANDLERFUNC) const uint8_t NAME = HandlerCount; data_collectors[HandlerCount++] = HANDLERFUNC
+#define RegisterHandler(NAME, HANDLERFUNC) \
+{ \
+    const uint8_t NAME = HandlerCount; \
+    data_collectors[HandlerCount++] = HANDLERFUNC; \
+}
 
 #define POST_TEMP_HUM
 #endif
